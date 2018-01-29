5f9a1125758df6d33844eb0f658bed543ba53dc1
Aulide81 <emiliojat@gmail.com>
2018-01-29 21:57:12 +0100

    Update desc.R

--- R/desc.R
+++ R/desc.R
@@ -1,3 +1,15 @@
 desc<-function(x,w,stat,dec){
   UseMethod("desc", x)
 }
+
+desc.data.frame<-function(x,w,stat,dec){
+  t(sapply(x,.desc,w,stat,dec))
+}
+
+desc.numeric<-function(x,w,stat,dec){
+ .desc(x,w,stat,dec)
+}
+
+desc.logical<-function(x,w,stat,dec){
+  .desc(x,w,stat,dec)
+}
