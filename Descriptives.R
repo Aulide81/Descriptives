c926c00e002124ebd9daec44269f4922138f955a
Aulide81 <emiliojat@gmail.com>
2018-02-06 23:19:37 +0100

    Update Descriptives.R

--- Descriptives.R
+++ Descriptives.R
@@ -238,6 +238,14 @@ if(is.list(x) & !missing(value)) {
   }
 }
 }
+             
+Count<-function(x,w){
+if(missing(w)){
+    sum(!is.na(x),na.rm=T)
+  }else{
+    sum(w[!is.na(x)],na.rm=T)
+  }
+}
 
 Sum<-function(x,w){
   if(missing(w)){
